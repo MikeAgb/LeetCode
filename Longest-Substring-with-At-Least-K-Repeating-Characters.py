5e37fa460a59a7c6f299e095edae70f9625d04eb
MikeAgb <46071825+MikeAgb@users.noreply.github.com>
2022-01-09 02:16:01 -0500

    Update Longest-Substring-with-At-Least-K-Repeating-Characters.py

--- Longest-Substring-with-At-Least-K-Repeating-Characters.py
+++ Longest-Substring-with-At-Least-K-Repeating-Characters.py
@@ -1,2 +1,12 @@
 class Solution:
     def longestSubstring(self, s: str, k: int) -> int:
+        # base condition
+        if len(s)==0:
+            return 0
+        count = 0
+        for c in s:   
+            if s.count(c)<k:
+                print(count)
+                return max(self.longestSubstring(s[:count],k),self.longestSubstring(s[count+1:],k))
+            count+=1
+        return len(s)
